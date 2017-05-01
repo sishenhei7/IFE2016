f2ca20528362525ad18c5f76b34d3733c513acc2
sishenhei7 <982505136@qq.com>
2017-05-01 21:31:27 +0800

    提交作业16 1.正则匹配，reg.test(),trim()去空格 2.最好不要出现alert 3.代码的写作风格——空格的运用 4.用javascript建立表格的几种方法 5.对象的属性的操作方法 6.响应事件 addEventListener,attachEvent 7.window.onload

--- js/task_1_16_1.js
+++ js/task_1_16_1.js
@@ -12,15 +12,49 @@ var aqiData = {};
  * 从用户输入中获取数据，向aqiData中增加一条数据
  * 然后渲染aqi-list列表，增加新增的数据
  */
-function addAqiData() {
-
+function addAqiData() {	
+	var aqiCityValue = document.getElementById("aqi-city-input").value.trim();
+	var aqiValueValue = document.getElementById("aqi-value-input").value.trim();
+	var aqiCityReg = /^[a-zA-Z\u4E00-\u9FA5]+$/ ;
+	var aqiValueReg = /^\d+$/ ;
+	if ( !aqiCityReg.test(aqiCityValue) ){
+		alert("城市名必须为中英文字符");
+	}else if ( !aqiValueReg.test(aqiValueValue) ){
+		alert("空气质量指数必须为整数");
+	}else {
+		if ( aqiCityValue ) {
+		aqiData[aqiCityValue] = aqiValueValue;
+	}}	
 }
 
 /**
  * 渲染aqi-table表格
  */
 function renderAqiList() {
-
+	var table = document.getElementById("aqi-table");
+	table.innerHTML = null;
+	var tbody = document.createElement("tbody");
+	table.appendChild(tbody);
+	tbody.insertRow(0);
+	tbody.rows[0].insertCell(0);
+	tbody.rows[0].cells[0].appendChild(document.createTextNode("城市"));
+	tbody.rows[0].insertCell(1);
+	tbody.rows[0].cells[1].appendChild(document.createTextNode("空气质量"));
+	tbody.rows[0].insertCell(2);
+	tbody.rows[0].cells[2].appendChild(document.createTextNode("操作"));
+	var i = 0;
+	for (data in aqiData){
+		tbody.insertRow(i+1);
+		tbody.rows[i+1].insertCell(0);
+		tbody.rows[i+1].cells[0].appendChild(document.createTextNode(data));
+		tbody.rows[i+1].insertCell(1);
+		tbody.rows[i+1].cells[1].appendChild(document.createTextNode(aqiData[data]));
+		tbody.rows[i+1].insertCell(2);
+		var aqiDataButton = document.createElement("button");
+		aqiDataButton.appendChild(document.createTextNode("删除"));
+		tbody.rows[i+1].cells[2].appendChild(aqiDataButton);
+		i++;
+		}
 }
 
 /**
@@ -28,18 +62,23 @@ function renderAqiList() {
  * 获取用户输入，更新数据，并进行页面呈现的更新
  */
 function addBtnHandle() {
-  addAqiData();
-  renderAqiList();
+	var aqiCity = document.getElementById("aqi-city-input");
+	var aqiValue = document.getElementById("aqi-value-input");
+	addAqiData();
+	renderAqiList();
+	aqiCity.value = "";
+	aqiValue.value = "";
 }
 
 /**
  * 点击各个删除按钮的时候的处理逻辑
  * 获取哪个城市数据被删，删除数据，更新表格显示
  */
-function delBtnHandle() {
+function delBtnHandle(event) {
   // do sth.
-
-  renderAqiList();
+	var city = event.target.parentNode.parentNode.firstChild.innerText;
+	delete aqiData[city];
+	renderAqiList();
 }
 
 function init() {
@@ -47,7 +86,24 @@ function init() {
   // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
 
   // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
-
+	var addBtn=document.getElementById("add-btn");
+	var addTable=document.getElementById("aqi-table");
+	var EventUtil={
+		addHandler:function(element,type,handler){
+			if (element.addEventListener){
+				element.addEventListener(type,handler,false);
+			} else if (element.attachEvent){
+				element.attachEvent("on"+type,handler);
+			} else {
+				element["on"+type]=handler;
+			}
+		},
+	};
+	EventUtil.addHandler(addBtn,"click",addBtnHandle);
+	EventUtil.addHandler(addTable,"click",delBtnHandle);
 }
 
-init();
+window.onload = function(){
+	init();
+}
+
