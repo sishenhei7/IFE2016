eb1c4e5b259df504251e0552724d077eb33bf212
sishenhei7 <982505136@qq.com>
2017-05-08 17:26:35 +0800

    提交作业25 1.textDecoration属性 2.js中判断color要有rgb 3.div的display是block不是box 4.边框有时能做一些不可思议的事情

--- js/task_1_25_1.js
+++ js/task_1_25_1.js
@@ -1,6 +1,5 @@
 //遍历动画辅助参数
-var i, delay, searchMark;
-var markList = [];
+var searchMark, markBox;
 
 //事件绑定函数，兼容浏览器差异
 var addEvent = function(element, event, listener) {
@@ -17,6 +16,9 @@ var addEvent = function(element, event, listener) {
 
 //创建节点树
 var createTree = function(Node){
+	if (Node == null) {
+		Node = document.getElementById("main");
+	}
 	addBox("Dom", Node);
 	addBox("apple", Node.children[0]);
 	addBox("pear", Node.children[0].children[0]);
@@ -34,50 +36,19 @@ var createTree = function(Node){
 	addBox("man", Node.children[1].children[0]);
 	addBox("haha", Node.children[1]);
 	addBox("be", Node.children[1].children[1]);
-	addBox("c", Node.children[1].children[1].children[0]);
+	addBox("cos", Node.children[1].children[1].children[0]);
 	addBox("grass", Node.children[1].children[1]);
 	addBox("fish", Node);
-}
-
-//前序遍历
-var preOrdering = function(Node){
-	i++;
-	setTimeout(function(){Node.style.backgroundColor = "rgb(204, 118, 216)";}, (2*i-1)*delay);
-	setTimeout(function(){Node.style.backgroundColor = "white";}, (2*i)*delay);
-	var j = 0;
-	while (Node.children[j]) {
-		if (Node.children[j].nodeName.toUpperCase() === "DIV"){
-			preOrdering(Node.children[j]);
-		}
-		j++;
-	}
-}
-
-//后序遍历
-var postOrdering = function(Node){
-	var j = 0;
-	while (Node.children[j]) {
-		if (Node.children[j].nodeName.toUpperCase() === "DIV"){
-			preOrdering(Node.children[j]);
-		}
-		j++;
-	}
-	i++;
-	setTimeout(function(){Node.style.backgroundColor = "rgb(204, 118, 216)";}, (2*i-1)*delay);
-	setTimeout(function(){Node.style.backgroundColor = "white";}, (2*i)*delay);
+	Node.style.background = null;
 }
 
 //查找节点
 var search = function(Node, searchText){
-	i++;
 	var nodeChildren = Node.children;
 	if (Node.firstChild && Node.firstChild.nodeValue.trim() == searchText) {
-		setTimeout(function(){Node.style.backgroundColor = "red";}, (2*i-1)*delay);
-		setTimeout(function(){Node.style.backgroundColor = "red";}, (2*i)*delay);
+		Node.style.textDecoration = "underline";
+		boxAppear(Node.parentNode);
 		searchMark = 1;
-	} else {
-		setTimeout(function(){Node.style.backgroundColor = "rgb(204, 118, 216)";}, (2*i-1)*delay);
-		setTimeout(function(){Node.style.backgroundColor = "white";}, (2*i)*delay);
 	}
 	var j = 0;
 	while (nodeChildren[j]) {
@@ -88,133 +59,222 @@ var search = function(Node, searchText){
 	}
 }
 
-var remove = function(markList, node){
-	var index = markList.lastIndexOf(node);
-	if (index >= 0){
-		markList = markList.splice(index, 1)
+//reset
+var resetBox = function(Node){
+	var j = 0;
+	var nodeChildren = Node.children;
+	if (Node.style.textDecoration === "underline") {
+		Node.style.textDecoration = "none";		
+	}
+	while (nodeChildren[j]) {
+		if (nodeChildren[j].nodeName.toUpperCase() === "DIV"){
+			resetBox(nodeChildren[j], searchText);
+		}
+		j++;
 	}
 }
 
-//添加节点 
+//添加一个字节点节点 
 var addBox = function(addText, parentNode){
 	var textNode = document.createTextNode(addText);
 	var boxNode = document.createElement("div");
-	var parentHeight = parentNode.offsetHeight;
-	var parentWidth = parentNode.offsetWidth;
-	boxNode.style.height = Math.round(parentHeight*0.8) + "px";
-	boxNode.style.display = "flex";
-	boxNode.style.border = "1px solid black";
+	boxNode.style.display = "block";
+	boxNode.style.border = "none";
+	boxNode.style.marginLeft = "10px";
 	boxNode.appendChild(textNode);
+	boxNode.style.color = "#c02c38";
+	boxNode.style.textAlign = "left";
+	boxNode.style.fontSize = "5px";
+	boxNode.style.borderLeft = "1px dotted red";
+	boxNode.style.background = "url(../photo/task_2_25_3.jpg) no-repeat top left";
+	boxNode.style.paddingLeft = "20px"
 	parentNode.appendChild(boxNode);
-//单击选定
-//	addEvent(boxNode, "click", function(event){
-//		var e = event || window.event;
-//		e.target.style.border = "3px dotted red";
-//		if (markList.indexOf(e.target) < 0){
-//			markList.push(e.target);
-//		}
-//	})
-//双击取消
-//	addEvent(boxNode, "dblclick", function(event){
-//		var e = event || window.event;
-//		e.target.style.border = "1px solid black";
-//		remove(markList, e.target);
-//	})
-//单击选定，单击取消，但是貌似有bug，我也不知道为什么
-//	addEvent(boxNode, "click", function(event){
-//		var e = event || window.event;
-//		if (markList.indexOf(e.target) < 0){
-//			markList.push(e.target);
-//			e.target.style.border = "3px dotted red";
-//		} else {
-//			remove(markList, e.target);
-//			e.target.style.border = "1px solid black";
-//			
-//		}
-//	})
+	parentNode.style.background = "url(../photo/task_2_25_1.jpg) no-repeat top left";
+}
+	
+//添加一个兄弟节点
+var addSiblingBox = function(addText, siblingNode){
+	var textNode = document.createTextNode(addText);
+	var boxNode = document.createElement("div");
+	boxNode.style.display = "block";
+	boxNode.style.border = "none";
+	boxNode.style.marginLeft = "10px";
+	boxNode.appendChild(textNode);
+	boxNode.style.color = "#c02c38";
+	boxNode.style.textAlign = "left";
+	boxNode.style.fontSize = "5px";
+	boxNode.style.borderLeft = "1px dotted red";
+	boxNode.style.background = "url(../photo/task_2_25_3.jpg) no-repeat top left";
+	boxNode.style.paddingLeft = "20px"
+	siblingNode.parentNode.insertBefore(boxNode, siblingNode.nextSibling);
+}
+
+//点击时其它项颜色重置
+var boxReset = function(Node){
+	var j = 0;	
+	Node.style.color = "#c02c38";
+	while (Node.children[j]) {
+		if (Node.children[j].nodeName.toUpperCase() === "DIV"){
+			boxReset(Node.children[j]);	
+		}
+		j++;
+	}
+}
+
+//隐藏子节点
+var boxDisappear = function(Node){	
+	var nodeLength = Node.children.length;
+	for (var i = 0; i < nodeLength; i++) {
+		Node.children[i].style.display = "none";
+	}
+}
+
+//显示子节点
+var boxAppear = function(Node){
+	var nodeLength = Node.children.length;
+	for (var i = 0; i < nodeLength; i++) {
+		Node.children[i].style.display = "block";
+	}
 }
 
 function init() {
-	var divOne = document.getElementById("div-one");
-	createTree(divOne);	
-	var preButton = document.getElementById("preorder");
-	var postButton = document.getElementById("postorder");
+	var start = new Date();
+	createTree();	
+	var main = document.getElementById("main");
 	var searchButton = document.getElementById("search-button");
-	var addButton = document.getElementById("add-button");
+	var resetButton = document.getElementById("reset-button");
+	var addChild = document.getElementById("add-child");
+	var addSibling = document.getElementById("add-sibling");
 	var removeButton = document.getElementById("remove-button");
-	addEvent(preButton, "click", function(){
-		i = 1;
-		delay = parseInt(document.getElementById("delay").value, 10);
-		preOrdering(divOne);
-	})
-	addEvent(postButton, "click", function(){
-		i = 1;
-		delay = parseInt(document.getElementById("delay").value, 10);
-		postOrdering(divOne);
-	})
+	
 	addEvent(searchButton, "click", function(){
-		i = 1;
+		var start = new Date(); 
 		searchMark = 0;
 		searchText = document.getElementById("search-text").value.trim();
-		delay = parseInt(document.getElementById("delay").value, 10);
-		search(divOne, searchText);
+		search(main, searchText);
 		if (searchMark === 0) {
 			setTimeout(function(){alert("很可惜没有找到！");}, (2*i+1)*delay);
 		}
+		var stop = new Date();
+		var time = "本次操作一共用了" + (stop - start) + "ms";
+		var body = searchButton.parentNode;
+		var loadTime = document.createElement("div");
+		loadTime.appendChild(document.createTextNode(time));
+		body.appendChild(loadTime);
+		loadTime.style.display = "inline" ;
+		loadTime.style.textAlign = "center" ;
+		loadTime.style.border = "none" ;
+		loadTime.style.color = "grey" ;	
+		setTimeout(function(){
+			body.removeChild(loadTime);
+		},3000)
 	})
-	addEvent(addButton, "click", function(event){
+	
+	addEvent(resetButton, "click", function(){
+		var start = new Date(); 
+		resetBox(main);
+		var stop = new Date();
+		var time = "本次操作一共用了" + (stop - start) + "ms";
+		var body = resetButton.parentNode;
+		var loadTime = document.createElement("div");
+		loadTime.appendChild(document.createTextNode(time));
+		body.appendChild(loadTime);
+		loadTime.style.display = "inline" ;
+		loadTime.style.textAlign = "center" ;
+		loadTime.style.border = "none" ;
+		loadTime.style.color = "grey" ;	
+		setTimeout(function(){
+			body.removeChild(loadTime);
+		},3000)
+	})
+	
+	addEvent(addChild, "click", function(event){
+		var start = new Date(); 
 		var addText = document.getElementById("add-text").value;
-		var markListCopy = markList;
-		markListLength = markListCopy.length;
-		if (markListLength > 0) {
-			for (var i = 0; i < markListLength; i++) {
-				addBox(addText, markListCopy[i]);
-			}
+		var markBoxCopy = markBox;
+		if (markBoxCopy) {
+			addBox(addText, markBoxCopy);
 		} else {
 			alert("请先选中节点!!");
 		}
+		var stop = new Date();
+		var time = "本次操作一共用了" + (stop - start) + "ms";
+		var body = addChild.parentNode;
+		var loadTime = document.createElement("div");
+		loadTime.appendChild(document.createTextNode(time));
+		body.appendChild(loadTime);
+		loadTime.style.display = "inline" ;
+		loadTime.style.textAlign = "center" ;
+		loadTime.style.border = "none" ;
+		loadTime.style.color = "grey" ;	
+		setTimeout(function(){
+			body.removeChild(loadTime);
+		},3000)
 	})
-	addEvent(removeButton, "click", function(event){
-		var markListCopy = markList;
-		markListLength = markListCopy.length;
-		if (markListLength > 0) {
-			for (var i = 0; i < markListLength; i++) {
-				markListCopy[i].parentNode.removeChild(markListCopy[i]);
-			}
-			markList = [];
+	
+	addEvent(addSibling, "click", function(event){
+		var start = new Date(); 
+		var addText = document.getElementById("add-text").value;
+		var markBoxCopy = markBox;
+		if (markBoxCopy) {
+			addSiblingBox(addText, markBoxCopy);
 		} else {
 			alert("请先选中节点!!");
 		}
+		var stop = new Date();
+		var time = "本次操作一共用了" + (stop - start) + "ms";
+		var body = addSibling.parentNode;
+		var loadTime = document.createElement("div");
+		loadTime.appendChild(document.createTextNode(time));
+		body.appendChild(loadTime);
+		loadTime.style.display = "inline" ;
+		loadTime.style.textAlign = "center" ;
+		loadTime.style.border = "none" ;
+		loadTime.style.color = "grey" ;	
+		setTimeout(function(){
+			body.removeChild(loadTime);
+		},3000)
 	})
-//给divOne绑定点击事件
-//	addEvent(divOne, "click", function(event){
-//		var e = event || window.event;
-//		e.target.style.border = "3px dotted red";
-//		if (markList.indexOf(e.target) < 0){
-//			markList.push(e.target);
-//		}
-//	})
-//	addEvent(divOne, "dblclick", function(event){
-//		var e = event || window.event;
-//		e.target.style.border = "1px solid black";
-//		remove(markList, e.target);
-//	})
-//单击选定，单击取消，但是貌似有bug，我也不知道为什么  
-	addEvent(divOne, "click", function(event){
+
+	addEvent(main, "click", function(event){
 		var e = event || window.event;
-		if (e.target.style.borderStyle == "solid"){
-			markList.push(e.target);
-			e.target.style.border = "3px dotted red";
+		if (e.target.style.color === "rgb(192, 44, 56)"){
+			boxReset(main);
+			markBox = e.target;
+			e.target.style.color = "#5ab43c";
 		} else {
-			remove(markList, e.target);
-			e.target.style.border = "1px solid black";
+			markBox = null;
+			boxReset(main);			
 		}
 	})
-// 绑定bug已修复。
-//原因有2个：
-//第一个是重复绑定；
-//第二个是JavaScript的样式和css样式独立，所以不能用javaScript语句来判断css样式
+	
+	addEvent(main, "dblclick", function(event){
+		var e = event || window.event;
+		var eLength = e.target.children.length;
+		if (eLength > 0 && e.target.children[0].style.display == "block"){
+			e.target.style.background = "url(../photo/task_2_25_2.jpg) no-repeat top left";
+			boxDisappear(e.target);
+		} else if (eLength > 0 && e.target.children[0].style.display == "none") {
+			e.target.style.background = "url(../photo/task_2_25_1.jpg) no-repeat top left";	
+			boxAppear(e.target);			
+		}
+	})
+	
+//性能测试
+	var stop = new Date();
+	var time = "本次网页加载一共用了" + (stop - start) + "ms";
+	var loadTime = document.createElement("div");
+	var body = document.getElementsByTagName("body")[0];
+	loadTime.appendChild(document.createTextNode(time));
+	body.appendChild(loadTime);
+	loadTime.style.textAlign = "center" ;
+	loadTime.style.border = "none" ;
+	loadTime.style.color = "grey" ;	
+	setTimeout(function(){
+		body.removeChild(loadTime);
+	},3000)
 }
+
 window.onload=function(){
 	init();
 };
