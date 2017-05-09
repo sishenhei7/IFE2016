f690c05fff2c0b4982f19265fe93f75cc6be14c5
sishenhei7 <982505136@qq.com>
2017-05-09 22:52:36 +0800

    提交作业30 1.表单各元素用数组储存 2.相同功能的函数可以用一个大函数封装起来 3.$ = function(el) {return document.querySelector(el);}   $$ = function(el) {return document.querySelectorAll(el);} 4.type="password" 5.border和outline的区别(重要) 6.<input type="button"> 和<button> 的区别之一是 <button>点击后自动清空内容。可以说<button>更智能。 7.box-shadow 8.用name，password等字命名变量可能会产生意想不到的坏结果， 因为貌似系统中有这个。

--- js/task_1_30_1.js
+++ js/task_1_30_1.js
@@ -1,3 +1,5 @@
+//验证标记变量
+var mark = [false, false, false, false, false];
 
 //封装getElementById
 var $ = function (id) {
@@ -17,50 +19,119 @@ function addEvent(element, event, listener) {
     }
 }
 
-//必填，长度为4-16个字符(汉字长度为2，其余为1)
+//验证不为空
+var checkNull = function(str){
+	var length01 = str.replace(/\s/g,'').length;
+	return (length01 > 0);
+}
+
+//验证开头(必须以汉字或字母开头)
+var checkHead = function(str){
+	var reg = /^[\u4e00-\u9fa5A-Za-z]/;
+	return reg.test(str);
+}
+
+//验证长度为4-16个字符(汉字长度为2，其余为1)
 var checkLength = function(str){
 	var length01 = str.length;
 	var length02 = str.replace(/[^\u4e00-\u9fa5]/g,'').length;
 	return ((length01 + length02 >= 4) && (length01 + length02 <= 16));
 }
 
-//不能为空
-var checkNull = function(str){
-	var length01 = str.replace(/\s/g,'').length;
-	return (length01 > 0);
+//验证名称
+var checkName = function(str){
+	var check = (checkNull(str) && checkHead(str) && checkLength(str));
+	mark[0] = check;
+	return check;
 }
 
-//格式正确(必须以汉字或字母开头)
-var checkFormat = function(str){
-	var reg = /^[\u4e00-\u9fa5A-Za-z]/;
+//验证密码
+var checkPassword = function(str){
+	var check = (checkNull(str) && checkLength(str));
+	mark[1] = check;
+	return check;
+}
+
+//验证密码确认
+var checkPasswordConfirm = function(str){
+	var gPassword = $('password').value;
+	var check = (checkNull(str) && checkLength(str) && (str === gPassword));
+	mark[2] = check;
+	return check;
+}
+
+//验证邮箱
+var checkMail = function(str){
+	var reg = /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/;
+	mark[3] = reg.test(str);
 	return reg.test(str);
 }
 
-//封装事件绑定函数，使输入合法时变绿色，不合法时变红色
-var addCheckEvent = function(element, event, checkFun, tip){
-	addEvent(element, event, function(){
+//验证11位手机号
+var checkCellphone = function(str){
+	var reg = /^1[0-9]{10}$/;
+	mark[4] = reg.test(str);
+	return reg.test(str);
+}
+
+
+//封装事件绑定函数，使有焦点时输入框变色，并出现提示
+var addFocusEvent = function(element){
+	addEvent(element, 'focus', function(){
+		element.style.border = '2px solid #4fc2d5';
+		element.style.boxShadow = '0px 0px 15px #4fc2d5';
+		element.parentNode.nextElementSibling.style.color = 'grey';			
+	});
+}
+
+//封装事件绑定函数，使焦点离开时检测，合法时变绿色，不合法时变红色
+var addBlurEvent = function(element, checkFun){
+	addEvent(element, 'blur', function(){
 		var check = checkFun(element.value);
+		element.style.boxShadow = 'none';
 		if (check) {
-			element.style.border = '2px solid green';
-			tip.style.color = 'green';
+			element.style.border = '2px solid #ded9d9';
+			element.parentNode.nextElementSibling.style.color = 'green';
+			element.parentNode.nextElementSibling.innerHTML = '格式正确';
 		} else {
 			element.style.border = '2px solid red';
-			tip.style.color = 'red';			
+			element.parentNode.nextElementSibling.style.color = 'red';			
 		}
 	});
 }
 
+//点击事件函数
+var buttonCheck = function(){
+	var i = 0;
+	while (mark[i]) {
+		i++;
+	}
+	if (i < 4) {
+		alert('表格还没填完整，提交失败');
+	} else {
+		alert('提交成功');
+	};
+}
+
 function init() {
-	var strLength = $('string-length');
-	var strNull = $('string-null');
-	var strFormat = $('string-format');
-	var lengthTip = $('length-tip');
-	var nullTip = $('null-tip');
-	var formatTip = $('format-tip');
-	addCheckEvent(strLength, 'blur', checkLength, lengthTip);
-	addCheckEvent(strNull, 'blur', checkNull, nullTip);
-	addCheckEvent(strFormat, 'blur', checkFormat, formatTip);
-	strLength.focus();
+	var gName = $('name');
+	var gPwd = $('password');
+	var gPwdConfirm = $('password-confirm');
+	var gMail = $('mail');
+	var gCellphone = $('cellphone');
+	var gComButton = $('commit');
+	addFocusEvent(gName);	
+	addFocusEvent(gPwd);
+	addFocusEvent(gPwdConfirm);
+	addFocusEvent(gMail);
+	addFocusEvent(gCellphone);
+	addBlurEvent(gName, checkName);
+	addBlurEvent(gPwd, checkPassword);
+	addBlurEvent(gPwdConfirm, checkPasswordConfirm);
+	addBlurEvent(gMail, checkMail);
+	addBlurEvent(gCellphone, checkCellphone);
+	addEvent(gComButton, 'click', buttonCheck);
+	gName.focus();
 }
 window.onload=function(){
 	init();
