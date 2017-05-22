c7e5af289e0994813ead23a97607accb455626dd
yangzhou <982505136@qq.com>
2017-05-22 22:15:23 +0800

    提交作业27 1.setTimeout会使代码块延迟进行，所以如果后面的代码需要用到这个代码块的参数，那么后面的也要延迟 2.单选框的选中状态.checked 3.删除数组的第x个元素：splice(x, 1)

--- js/task_1_27_1.js
+++ js/task_1_27_1.js
@@ -16,6 +16,34 @@ function addEvent(element, event, listener) {
     }
 }
 
+//返回标签下选中的单选选项
+var getCheckedInput = function(id){
+	var label = $(id);
+	var list = label.getElementsByTagName('input');
+	for (var i = 0, j = list.length; i < j; i++) {
+		if (list[i].checked) {
+			return list[i].value;
+		}
+	}
+}
+
+//动力和能源系统标签的解析器
+var parsePowerAndEnergySystem = function (value) {
+	switch(value) {
+		case 'qianjin' :
+			return ['ship-qianjin', 0.05];
+		case 'benteng' :
+			return ['ship-benteng', 0.07];
+		case 'chaoyue' :
+			return ['ship-chaoyue', 0.09];
+		case 'jinliang' :
+			return 0.02;
+		case 'guangneng' :
+			return 0.03;			
+		case 'yongjiu' :
+			return 0.04;						
+	}
+}
 
 //飞船对象
 var ship = function(number){
@@ -30,7 +58,7 @@ var ship = function(number){
 	this.energyInterval = undefined;
 	var that = this;
 	
-	function init(){
+	function createAirship(){
 		var universe = $("universe");
 		var ship = document.createElement('div');
 		ship.className = 'ship';
@@ -43,6 +71,25 @@ var ship = function(number){
 		ship.appendChild(document.createTextNode(shipText));
 		that.airship = ship;
 	};
+	
+	function loadPowerSystem(){
+		var powerSystemValue = getCheckedInput("power-system");
+		var powerSystemSet = parsePowerAndEnergySystem(powerSystemValue);
+		that.airship.className = powerSystemSet[0];
+		that.energyConsumption = powerSystemSet[1];
+	};
+	
+	function loadEnergySystem(){
+		var energySystemValue = getCheckedInput("energy-system");
+		var energySystemSet = parsePowerAndEnergySystem(energySystemValue);
+		that.energyProduction = energySystemSet;
+	};	
+	
+	function init() {
+		createAirship();
+		loadPowerSystem();
+		loadEnergySystem();
+	}
 	
 	init();
 };
@@ -62,13 +109,9 @@ ship.prototype.powerSystem = function() {
 		that.airship.style.animationPlayState = 'paused';
 		that.state = 'paused';
 		window.clearInterval(that.energyInterval);
-	}
+	};
 	return {startFlight, stopFlight};
 };
-
-//var aa = new ship;
-//aa.powerSystem.startFlight();
-
 
 //能源系统
 ship.prototype.energySystem = function() {
@@ -93,17 +136,50 @@ ship.prototype.energySystem = function() {
 ship.prototype.signalSystem = function() {
 	var that = this;
 	var getSignal = function(signal){
+		signal = that.signalSystem().adapter(signal);
 		if (that.shipId === signal.id) {
-			if (signal.command === 'start') {
+			if (signal.command === 'start' && that.state === 'paused') {
 				that.powerSystem().startFlight();
-			} else if (signal.command === 'stop') {
+			} else if (signal.command === 'stop' && that.state === 'running') {
 				that.powerSystem().stopFlight();
 			} else if (signal.command === 'destroy') {
 				that.selfSystem().selfDestroy();
 			}
 		}
 	};
-	return {getSignal};
+	
+	var adapter = function(signal){
+		var shipIdBinary = signal.slice(0,4);
+		var shipCommandBinary = signal.slice(4,8);
+		var id,command;
+		switch(shipIdBinary){
+			case '0001' :
+				id = 1;
+				break;
+			case '0010' :
+				id = 2;
+				break;
+			case '0100' :
+				id = 3;
+				break;
+			case '1000' :
+				id = 4;
+				break;
+		};
+		switch(shipCommandBinary){
+			case '0001' :
+				command = 'start';
+				break;
+			case '0010' :
+				command = 'stop';
+				break;
+			case '1100' :
+				command = 'destroy';
+				break;
+		};
+		return {id, command};
+	}
+	return {getSignal, adapter};
 };
 
 //自爆系统
@@ -120,6 +196,7 @@ ship.prototype.selfSystem = function() {
 commander = {
 	ships: [],
 	shipNumber: 0,
+	shipIdList: [],
 }
 
 //模仿丢包率
@@ -132,18 +209,72 @@ var packetLoss = function(packetLossRate){
 	}
 }
 
+//指令转化为二进制
+var commanderAdapter = function(shipcommand) {
+	var signal = [];
+	switch(shipcommand.id){
+		case  1 :
+			signal[0] = '0001';
+			break;
+		case  2 :
+			signal[0] = '0010';
+			break;
+		case  3:
+			signal[0] = '0100';
+			break;
+		case  4 :
+			signal[0] = '1000';
+			break;
+	};
+	switch(shipcommand.command){
+		case 'start' :
+			signal[1] = '0001';
+			break;
+		case 'stop' :
+			signal[1] = '0010';
+			break;
+		case 'destroy':
+			signal[1] = '1100';
+			break;
+	};
+	signal = signal.join('');
+	return signal;
+}
+
+//显示传输成功或者失败
+var showWhetherSpread = function(spread){
+	var consoleArea = $('console-area');
+	var showInformation = document.createElement('div');
+	if (spread) {
+		showInformation.appendChild(document.createTextNode('传输成功'));
+	} else {
+		showInformation.appendChild(document.createTextNode('传输失败，尝试重新传输'));
+	}
+	consoleArea.appendChild(showInformation);
+	setTimeout(function(){
+		consoleArea.removeChild(consoleArea.children[0]);
+	}, 3000);
+}
+
 //给所有飞船下达命令
-var controlAllAirship = function(shipcommand){
-	if (packetLoss(0.3)) {
-		for (var i = 0, j = commander.ships.length; i < j; i++) {
-			commander.ships[i].signalSystem().getSignal(shipcommand);
-		}
+var spreadMediumBus = function(shipcommand){
+	var signal = commanderAdapter(shipcommand);
+	if (packetLoss(0.1)) {
+		setTimeout(function(){
+			for (var i = 0, j = commander.ships.length; i < j; i++) {
+				commander.ships[i].signalSystem().getSignal(signal);
+			}
+		}, 300);
+		showWhetherSpread(true);
+	} else {
+		showWhetherSpread(false);
+		spreadMediumBus(shipcommand);
 	}
 }
 
 //发射飞船后建立表单
 var createAirshipForm = function (shipNumber) {
-	var commandArea = $('command-area');
+	var commandSubarea = $('command-subarea');
 	var arshipControlDiv = document.createElement('div');
 	arshipControlDiv.appendChild(document.createTextNode('对' + shipNumber + '号飞船下达命令:'));
 	var arshipStart = document.createElement('button');
@@ -155,19 +286,23 @@ var createAirshipForm = function (shipNumber) {
 	arshipControlDiv.appendChild(arshipStart);
 	arshipControlDiv.appendChild(arshipStop);
 	arshipControlDiv.appendChild(arshipDestroy);
-	commandArea.appendChild(arshipControlDiv);
+	commandSubarea.appendChild(arshipControlDiv);
 	
 	addEvent(arshipStart, 'click', function(){
-		controlAllAirship({id: shipNumber,command: 'start'});
+		spreadMediumBus({id: shipNumber,command: 'start'});
 	});
 	
 	addEvent(arshipStop, 'click', function(){
-		controlAllAirship({id: shipNumber,command: 'stop'});
+		spreadMediumBus({id: shipNumber,command: 'stop'});
 	});
 	
 	addEvent(arshipDestroy, 'click', function(){
-		controlAllAirship({id: shipNumber,command: 'destroy'});
-		commandArea.removeChild(arshipControlDiv);
+		spreadMediumBus({id: shipNumber,command: 'destroy'});
+		commandSubarea.removeChild(arshipControlDiv);
+		setTimeout(function(){
+			commander.ships.splice(commander.shipIdList.indexOf(shipNumber), 1);
+			commander.shipIdList.splice(commander.shipIdList.indexOf(shipNumber), 1);
+		}, 300);
 		commander.shipNumber--;
 	});
 }
@@ -176,9 +311,16 @@ var createAirshipForm = function (shipNumber) {
 commander.launchAirship = function(){
 	if (commander.shipNumber < 4) {
 		commander.shipNumber++;
-		airship = new ship(commander.shipNumber);
+		var newShipNumber;
+		for (var i = 4; i >= 1; i--) {
+			if (commander.shipIdList.indexOf(i) === -1) {
+				newShipNumber = i;
+			}
+		}
+		airship = new ship(newShipNumber);
+		commander.shipIdList.push(newShipNumber);
 		commander.ships.push(airship);
-		createAirshipForm(commander.shipNumber);
+		createAirshipForm(newShipNumber);
 	} else {
 		alert('您最多只能发射4只飞船！');
 	}	
