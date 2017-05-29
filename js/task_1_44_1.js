686a017d1cb983e484c79a981f1d44539d3b8b37
yangzhou <982505136@qq.com>
2017-05-30 03:41:28 +0800

    提交作业44 1.ajax学习网站：http://www.w3school.com.cn/ajax/index.asp 还蛮有启发意义的 2.ajax实现动态加载txt,php,xml文件 3.安装本地服务器还是出现跨域问题：Cross origin requests are only supported for protocol schemes的解决方法： 举个例子：在浏览器中把file:///D:/AppServ/www/php/new%2011411.html改写成http://localhost/php/new%2011411.html 因为其实你打开的是C盘中的html文件，不是服务器中的html文件，所以再引用服务器中的别的文件当然就跨域了 （网上的设置浏览器安全问题基本可以不看） 4.怎么看是否安装有本地服务器：浏览器输入localhost看能不能打开 5.js不允许访问本地文件，原因为如果能访问的话，那谁还敢打开网站呢哈哈 6.图片点击的时候全屏显示该图的动画效果 7.貌似有的浏览器会自动延迟加载图片，所以与图片有关的js最好不要用匿名函数的形式加载 8.Math.round()方法都能返回函数的代码 9.判断滚轮是否滚动到底部(当然还有其他方面的判断)： document.body.scrollTop > (document.body.scrollHeight - document.body.clientHeight - 100) 10.transform 属性和animation属性 11.-ms-, -moz-, -webkit-, -o- (-ms-有待考证) 12.overflow属性 13.js点击图片全屏显示伴随动画的写法

--- js/task_1_44_1.js
+++ js/task_1_44_1.js
@@ -1,7 +1,10 @@
+//这是gallery库
+function init() {
+//(function() {
 //封装getElementById
 var $ = function (id) {
 	return document.getElementById(id);
-}
+};
 
 //事件绑定函数，兼容浏览器差异
 function addEvent(element, event, listener) {
@@ -19,21 +22,158 @@ function addEvent(element, event, listener) {
 //创建一个类型为type的元素，内容为str，加在ele的子节点后面
 var createChildElement = function(type, str, ele){
 	var myEle = document.createElement(type);
-	if (str !== '') {
+	
+	if (str !== undefined && str !== '') {
 		myEle.appendChild(document.createTextNode(str));
 	}
 	ele.appendChild(myEle);
 	return myEle;
+};
+
+//配置左右间距，下间距，栏目数和栏目宽度（栏目长度是自适应的）
+var myMarginLeftAndRight = 16;
+var myMarginBottom = 16;
+var myColumn = 6;
+var myColumnWidth = 200;  //单位是px
+
+//将原图片按比例缩放，使他们的宽度和自定义的一样
+function myScaleTool (ele) {
+	var myHeight = ele.offsetHeight;
+	ele.style.height = Math.round((myColumnWidth/(ele.offsetWidth)) * myHeight) + 'px';
+	ele.style.width = myColumnWidth + 'px';
+	return ele.style.height;
 }
 
+//初始化每栏图片的起始位置
+var albumLefts = [];
+var albumTops = [];
+for (var i = 0; i < myColumn; i++) {
+	albumLefts[i] = myMarginLeftAndRight * 0.5 * (i * 2 + 1) + myColumnWidth * i;
+	albumTops[i] = 0;
+}
 
-function init() {
+//选取数组中的最小值，返回最小值序号
+function selectTheSmallest(array) {
+	var min = array[0];
+	var index = 0;
+	
+	for (var i = 1, j = array.length; i < j; i++) {
+		if (min > array[i]) {
+			min = array[i];
+			index = i;
+		}
+	}
+	
+	return index;
+}
 
+//更新每栏图片的起始位置
+function updateLeftsAndTops (ele) {
+	var myTop = selectTheSmallest(albumTops);
+	ele.style.left = albumLefts[myTop] + 'px';
+	ele.style.top = albumTops[myTop] + 'px';
+	
+	var height = myScaleTool(ele);
+	albumTops[myTop] = albumTops[myTop] + parseInt(height.slice(0, -2), 10) + myMarginBottom;	
+}
+
+//将图片按瀑布排列
+(function waterFall () {
+	$('album-waterfall').style.position = 'relative';
+	var myImgs = $('album-waterfall').getElementsByTagName('img');
+
+	for (var i = 0, j = myImgs.length; i < j; i++) {
+		myImgs[i].style.position = 'absolute';
+		updateLeftsAndTops(myImgs[i]);
+		addEvent(myImgs[i], 'click', function(event){
+			var e = event || window.event;
+			fullScreen(e.target.src);
+		});
+	}
+})();
+
+//图片点击的时候全屏显示该图
+function fullScreen (src) {
+	var wrapper = createChildElement('div', '', $('main'));
+	wrapper.className = 'full-screen-wrapper';
+	
+	var img = createChildElement('img', '', $('main'));
+	img.src = src;
+	img.className = 'full-screen-photo';
+	
+	$('main').style.overflow  = 'hidden';
+	
+	addEvent(wrapper, 'click', function(){
+		$('main').removeChild(wrapper);
+		$('main').removeChild(img);
+		$('main').style.overflow  = 'auto';
+	});
+}
+
+//根据一个数组给album-waterfall添加图片
+function addPhoto (array) {
+	var album = $('album-waterfall');
+
+	for (var i = 0, j = array.length; i < j; i++) {
+		var img = createChildElement('img', '', album);
+		img.src = '../photo/' + array[i];
+		img.style.position = 'absolute';
+		updateLeftsAndTops(img);
+		
+		addEvent(img, 'click', function(event){
+			var e = event || window.event;
+			fullScreen(e.target.src);
+		});
+	}
+}
+
+//ajax动态加载
+function loadXMLDoc(){
+	var xmlhttp;
+
+	if (window.XMLHttpRequest){   // code for IE7+, Firefox, Chrome, Opera, Safari
+	  xmlhttp = new XMLHttpRequest();
+	
+	} else {     // code for IE6, IE5
+	  xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	  
+	xmlhttp.onreadystatechange=function(){
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+			var srcs = eval(xmlhttp.responseText);
+			addPhoto (srcs);
+		}
+	}
+	
+	xmlhttp.open("GET","task_1_44_1.txt",true);
+	xmlhttp.send();
+}
+
+//当滚轮滑到最低端时启动自动加载
+addEvent(document, 'scroll', function(event){
+			if (document.body.scrollTop > (document.body.scrollHeight - document.body.clientHeight - 100)) {
+				loadXMLDoc();
+			}
+		});
+//})();
 }
 
 window.onload=function(){
 	init();
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
